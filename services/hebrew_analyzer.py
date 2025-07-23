72e4d0dc8d3dbc8128f433cbf84a0bc89f4eefaa
Davidi18 <165849027+Davidi18@users.noreply.github.com>
2025-07-24 01:03:56 +0300

    Update hebrew_analyzer.py

--- services/hebrew_analyzer.py
+++ services/hebrew_analyzer.py
@@ -13,7 +13,7 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-from models.hebrew_loader import hebrew_loader
+from models.hebspacy_loader import hebrew_loader
 
 
 class HebrewSemanticAnalyzer:
