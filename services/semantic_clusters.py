24f7c260b778cfb5a8689fed60420663327530a3
Davidi18 <165849027+Davidi18@users.noreply.github.com>
2025-07-24 12:39:14 +0300

    Update semantic_clusters.py

--- services/semantic_clusters.py
+++ services/semantic_clusters.py
@@ -357,7 +357,10 @@ class SemanticClusteringService:
         
         for i in range(len(keywords)):
             for j in range(i + 1, len(keywords)):
-                similarities.append(self._are_morphologically_related(keywords[i]['text'], keywords[j]['text']))
+        # Fix: keywords contain 'keyword' field, not 'text'
+                word1 = keywords[i].get('keyword', keywords[i].get('text', ''))
+                word2 = keywords[j].get('keyword', keywords[j].get('text', ''))
+                similarities.append(self._are_morphologically_related(word1, word2))
         
         return np.mean(similarities) if similarities else 0.0
     
